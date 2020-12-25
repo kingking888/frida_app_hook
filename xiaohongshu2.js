769461473e84dc9081da5f0a52f359223e2a9ec3
袁浪 <yuanlang@bigsfintech.com>
2020-12-25 14:20:08 +0800

    sheild

--- xiaohongshu2.js
+++ xiaohongshu2.js
@@ -1,3 +1,4 @@
+// 小红书 v5.26.2
 // frida -U -l xiaohongshu2.js -f com.xingin.xhs --no-pause
 function abc(){
     var base_address=Module.findBaseAddress('libshield.so')
@@ -44,17 +45,102 @@ function abc(){
 
 //abc();
 Java.perform(function () {
-    var OkHttpClient = Java.use("okhttp3.OkHttpClient");
-    OkHttpClient.newCall.implementation = function (request) {
-        if(request.url().toString().indexOf("https://www.xiaohongshu.com/api/sns/v2/note/user") != -1){
-            console.log(request);
-            console.log(request.toString());
-            console.log(request.headers().toString());
-            console.log(request.body());
-        }
-        var result = this.newCall(request);
+
+//    小红书hook
+//    var OkHttpClient = Java.use("okhttp3.OkHttpClient");
+//    OkHttpClient.newCall.implementation = function (request) {
+//        if(request.url().toString().indexOf("/api/sns/v3/user/") != -1){
+//            console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+//            console.log(request);
+//            console.log(request.toString());
+//            console.log(request.headers().toString());
+//            console.log(request.body());
+//        }
+//        var result = this.newCall(request);
+//        return result;
+//    }
+
+//    var ProfileDataProvider = Java.use("com.xingin.profile.newprofile.viewmodel.ProfileDataProvider");
+//    ProfileDataProvider.a.overload('java.lang.String').implementation = function (uid) {
+//        console.log(uid)
+//        console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+//        var result = this.b(uid);
+//        return result;
+//    }
+
+//    参数拼装
+//    var a = Java.use("com.xingin.shield.http.a");
+//    a.a.overload('java.util.Map').implementation = function (map) {
+//        var result = this.a(map)
+//        return result
+//    }
+//    var StringBuilder = Java.use("java.lang.StringBuilder");
+//    StringBuilder.toString.overload().implementation = function () {
+//        var result = this.toString()
+//        if (result.indexOf("note/user")!=-1){
+//            console.log(result)
+//        }
+//        return result
+//    }
+
+//    获取参数
+    var RedHttpInterceptor = Java.use("com.xingin.shield.http.RedHttpInterceptor");
+    RedHttpInterceptor.sessionId.implementation = function () {
+        var result = this.sessionId();
+        console.log("sessionId="+result)
         return result;
     }
-})
+    RedHttpInterceptor.deviceId.implementation = function () {
+        var result = this.deviceId();
+        console.log("deviceId="+result)
+        return result;
+    }
+    RedHttpInterceptor.userAgent.implementation = function () {
+        var result = this.userAgent();
+        console.log("userAgent="+result)
+        return result;
+    }
+    var String = Java.use("java.lang.String");
+    RedHttpInterceptor.getBytesOfParams.overload('okhttp3.Request').implementation = function (request) {
+        var result = this.getBytesOfParams(request);
+        var result2 = String.$new(result)
+        console.log("getBytesOfParams="+result2)
+        return result;
+    }
 
+    // 加密sign打印
+//    var i = Java.use("com.xingin.skynet.i");
+//    i.a.overload('java.util.Map').implementation = function (arg1) {
+//        var result = "";
+//        var keyset = arg1.keySet();
+//        var it = keyset.iterator();
+//        while(it.hasNext()){
+//            var keystr = it.next().toString();
+//            var valuestr = arg1.get(keystr).toString();
+//            var map= keystr+'='+valuestr+'&';
+//            result+=map
+//        }
+//        send(result);
+//        var result = this.a(arg1);
+//        send("sing:"+result);
+//        return result;
+//    }
+//    var ac = Java.use("com.xingin.common.util.ac");
+//    ac.a.overload('java.lang.String','java.lang.String').implementation = function (arg1,arg2) {
+//        console.log(arg1)
+//        console.log(arg2)
+//        var result = this.a(arg1,arg2);
+//        console.log(result)
+//        return result
+//    }
+
+        // 上传手机号加密
+//    var w = Java.use("com.xingin.common.util.w");
+//    w.a.overload('java.lang.String','java.lang.String').implementation = function (arg1, arg2) {
+//        console.log(arg1)
+//        console.log(arg2)
+//        var result = this.a(arg1,arg2);
+//        return result;
+//    }
+})
 
